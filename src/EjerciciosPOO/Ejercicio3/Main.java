4b4b9c7fbcf84d3e63b5a12e7edfc2a023de79e4
LUPUS23 <antoniodaw23@gmail.com>
2025-01-28 13:42:23 +0100

    Update

--- src/EjerciciosPOO/Ejercicio3/Main.java
+++ src/EjerciciosPOO/Ejercicio3/Main.java
@@ -3,9 +3,12 @@ package EjerciciosPOO.Ejercicio3;
 public class Main {
     public static void main(String[] args) {
         Cuenta c = new Cuenta("Josefina", 1200);
+        Cuenta c2 = new Cuenta("Paca", 800);
 
         c.retirar(200);
+        c2.retirar(1000);
 
         System.out.println(c.getCantidad());
+        System.out.println(c2.getCantidad());
     }
 }
